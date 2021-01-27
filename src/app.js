b8665b44ab0aebed7e5b9b0fc95d10e3bb07fdb1
mainara07 <assis.mainara@gmail.com>
2021-01-27 00:21:27 -0300

    *metodos, +DAO

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const usuarioController = require('./controllers/usuario-controller.js') //importando o que esta c/ *exports* no arquivo
-const tarefaController = require('./controllers/tarefa-controller.js') //importando o que esta c/ *exports* no arquivo
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const usuarioController = require('./controllers/usuario-controller.js'); //importando o que esta c/ *exports* no arquivo
+const tarefaController = require('./controllers/tarefa-controller.js'); //importando o que esta c/ *exports* no arquivo
+
+const Usuario = require('./models/usuario');
+const Tarefa = require('./models/tarefa');
+
+const sqlite = require ('sqlite3').verbose();
+const bd = require ('./infra/sqlite-db')
+
 const app = express()
 const port = 8080
 
-usuarioController(app)
-tarefaController(app)
+
+usuarioController(app,bd)
+tarefaController(app,bd)
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
