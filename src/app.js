152fc19dd4caebc15bd5ba4f3596708c7a6f3502
mainara07 <assis.mainara@gmail.com>
2021-02-16 19:00:48 -0300

    *dao *controller

--- src/app.js
+++ src/app.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 8080;
 
 const usuarioController = require('./controllers/usuario-controller.js'); //importando o que esta c/ *exports* no arquivo
 const tarefaController = require('./controllers/tarefa-controller.js'); //importando o que esta c/ *exports* no arquivo
 
-const Usuario = require('./models/usuario');
-const Tarefa = require('./models/tarefa');
-
-const sqlite = require ('sqlite3').verbose();
 const bd = require ('./infra/sqlite-db')
 
 const app = express()
-const port = 8080
-
+app.use(bodyParser.json());
 
 usuarioController(app,bd)
 tarefaController(app,bd)
-
-app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,36 +1,66 @@
-const Usuario = require ('../models/usuario')
-const UsuarioDAO = require ('../DAO/usuario-dao')
+const usuarioDao = require ('../DAO/usuario-dao')
 
 module.exports = (app,bd)=> {
-
-  const usuarioDAO = new UsuarioDAO(bd);
-
-  app.get('/usuario', (req, resp) => {
-    usuarioDAO.listaUsuario()
-    .then((usuario) => {
-      resp.send(usuario)
-    })
-    .catch((erro) =>{
+  const daoUsuario = new usuarioDao(bd);
+  
+  //Listagem de todos os usuários
+  app.get('/usuario', async (req, resp) => {
+    try {
+    let respLista = await daoUsuario.listaUsuario()
+    resp.send(respLista) 
+  } catch(error) {
+    resp.send(error)
+  }
+});
+  //Listagem de um único usuário com base em um parâmetro de rota
+  app.get('/usuario/:id', async (req, resp) => {
+    try {
+      const respBusca = await daoUsuario.buscaUsuario(req.params.id)
+      resp.send(respBusca)
+    } catch (erro) {
       resp.send(erro)
-    })
-  })
-
-  app.get('/usuario/:nome', (req,resp)=> {
-    for (let usr of bd.usuarios){
-      if (usuario.email== req.params.email){
-        resp.send(usuario);
-      }
+    }
+  });
+  // Inserção de um usuário
+  app.post('/usuario', async (req, resp)=> {
+    try {
+      let dados  = [
+        req.body.nome,
+        req.body.email, 
+        req.body.senha
+      ];
+      const respAdiciona = await daoUsuario.adicionaUsuario(dados)
+      resp.send(respAdiciona)
+    } catch (error){
+      resp.send(error)
     }
   });
 
-  app.post('/usuario', (req, resp)=> {
-    bd.run('INSERT INTO USUARIOS(NOME,EMAIL,SENHA) VALUES (?,?,?)', [req.body.nome,req.body.email,req.body.senha]), 
-    function (err) {
-      if (err){
-        throw new Error (`Erro ao tentar inserir ${err}`);
-      } else {
-        resp.send("Usuario adicionado ao banco")
-      }
-    } 
+  // Modificação de um usuário
+  app.put('/usuario/:id', async(req, resp)=> {
+    let dados = [
+      req.body.nome, 
+      req.body.email, 
+      req.body.senha,
+      req.params.id
+      ]
+    try{
+      const respModificaUsr= await daoUsuario.atualizaUsr(dados)
+      resp.send(respModificaUsr)
+    } catch (erro) {
+      resp.send(erro)
+    }
   });
+  // Deleção de um usuários
+  app.delete('/usuario/:id', async (req, resp) => {
+    try{
+      const respDeleta = await daoUsuario.deletaUsr(req.params.id)
+      resp.send(respDeleta)
+  
+    } catch (erro){
+      resp.send(erro)
+  
+    }
+  });
+  
 }
